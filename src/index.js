d06433353fd04c9b458e8aca644d2123fa9b0349
unknown <mbfawwaz@gmail.com>
2022-05-12 12:20:50 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Sample extends React.Component {
+constructor(props)
+{
+  super(props)
+  // this.username = props.name
+  // this.userage = props.Age
+  //   console.log('Constructor Called :' + props.name);
+  //   this.state = {
+  //     fruit : 'apple',
+  //     color : 'red'
+  //   }
+    this.state={value:""}
+}
+
+  // display = () => {
+  //   console.log('Display Method Called')
+  // }
+
+  // Changename = () => {
+  //   this.setState ({fruit: 'pomo'})
+  // }
+
+  oninputchange = (event) => {
+    this.setState({
+      value : event.target.value
+    })
+  }
+
+  render()
+  {
+    return <>
+      <h1>Duplicate Input Box</h1>
+      <div className='prebox'>
+      <input name='box1' type='text' placeholder="Enter Your Text Here" onInput={this.oninputchange} className='firstbox'></input>
+      <input value={this.state.value} placeholder="Content Will Be Displayed Here" className='secondbox' type='text' readOnly></input></div>
+    </>
+  }
+} 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    (<Sample name='Fawwaz' Age='25'/>)  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
